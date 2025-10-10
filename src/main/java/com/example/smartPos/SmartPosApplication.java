891213d2cf770a22ab90d0d4ec5da066e098e87b
sasindu.paththamperuma@gmail.com <sasindu.paththamperuma@gmail.com>
2025-10-11 00:09:56 +0530

    timezone issue

--- src/main/java/com/example/smartPos/SmartPosApplication.java
+++ src/main/java/com/example/smartPos/SmartPosApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.TimeZone;
 //import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -11,6 +13,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 public class SmartPosApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Colombo"));
 		SpringApplication.run(SmartPosApplication.class, args);
 	}
 
